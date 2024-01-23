6535194b29f1ddb3162219da81ba61f93c7676da
yesrin <93201736+yesrin@users.noreply.github.com>
2024-01-23 23:28:49 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,6 @@
+def solution(num_list):
+    if num_list[-1] > num_list[-2]:  # 마지막 원소가 그전 원소보다 크다면
+        num_list.append(num_list[-1] - num_list[-2])  # 마지막 원소에서 그전 원소를 뺀 값을 추가
+    else:  # 마지막 원소가 그전 원소보다 크지 않다면
+        num_list.append(num_list[-1] * 2)  # 마지막 원소를 두 배한 값을 추가
+    return num_list
