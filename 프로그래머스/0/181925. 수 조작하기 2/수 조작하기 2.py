85c83f992ef8fab4c72366baa0d8955575672575
yesrin <93201736+yesrin@users.noreply.github.com>
2024-01-25 22:55:18 +0900

    [level 0] Title: 수 조작하기 2, Time: 20.90 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,12 +1,12 @@
 def solution(numLog):
-    answer=""
-    for i in range(len(numLog)-1):
-        if numLog[i+1]-numLog[i]==1:
-            answer+="w"
-        elif numLog[i+1]-numLog[i]==-1:
-            answer+="s"
-        elif numLog[i+1]-numLog[i]==10:
-            answer+="d"
-        elif numLog[i+1]-numLog[i]==-10:
-            answer+="a"
+    answer = ""  # 결과를 저장할 문자열 초기화
+    for i in range(1, len(numLog)):  # 범위를 1부터 len(numLog)까지로 수정
+        if numLog[i] - numLog[i-1] == 1:
+            answer += "w"
+        elif numLog[i] - numLog[i-1] == -1:
+            answer += "s"
+        elif numLog[i] - numLog[i-1] == 10:
+            answer += "d"
+        elif numLog[i] - numLog[i-1] == -10:
+            answer += "a"
     return answer
